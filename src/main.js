03e38e9d903b05451719b8a566730e4014999fa7
qinchunbang <373413704@qq.com>
2020-06-19 15:19:14 +0800

    首页

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { get, post} from './utils/ajax'
+import './config/rem'
 
 Vue.config.productionTip = false
 
-new Vue({
+Vue.prototype.$http = {get,post};
+
+export default new Vue({
+  el:'#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+});
